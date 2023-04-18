e0cca0f9c4597f2114578c379a46a869dd0fa151
森元心 <shinshinshin0914@gmail.com>
2023-04-18 13:52:00 +0900

    first commit

---
+++ train-wikiFAmodel-main/train_wikiFA_by_tensorflow.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+def main():
+    # load training data
+    X_train, y_train, X_test, y_test = load_dataset()
+
+    # settings
+    expt_name = "M1_dim100"
+    input_dim = X_train.shape[1]
+    output_dim = y_train.shape[1]
+    activation = 'linear'
+    loss = 'mean_squared_error'
+    optimizer = 'adam'
+    epochs = 1000
+    # dropout
+
+    expt_name = expt_name + "_" + optimizer
+
+    # Define the model
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(output_dim, input_shape={
+              input_dim, }, use_bias=False, activation=activation))
+    # Compile the model
+    model.compile(optimizer=optimizer, loss=loss)
+    # Train the model
+    history = model.fit(X_train, y_train, epochs=epochs, validation_data=(
+        X_test, y_test), callbacks=[tf.keras.callbacks.History()])
+    model.save(f'models/{expt_name}.h5')
+
+    # Predict using the model
+    y_pred_train = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+
+    # Calculate cosine similarity between predicted and actual values
+    cos_sim_train = np.array([cosine_similarity(
+        y_pred_train[i], y_train[i]) for i in range(len(y_pred_train))])
+    cos_sim_test = np.array([cosine_similarity(
+        y_pred_test[i], y_test[i]) for i in range(len(y_pred_test))])
+
+    print(f"sim average train: {np.sum(cos_sim_train) / len(cos_sim_train)}")
+    print(f"sim average test: {np.sum(cos_sim_test) / len(cos_sim_test)}")
+
+    # グラフを描画
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    # fig, axs = plt.subplots(1, 3, figsize=(21, 7))
+    axs[0].plot(history.history['loss'], label='train_loss')
+    axs[0].plot(history.history['val_loss'], label='test_loss')
+    axs[0].set_title(f'Training Loss')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_ylabel('Loss')
+    axs[0].legend()
+
+    axs[1].plot(cos_sim_train, label='sim_train')
+    axs[1].plot(cos_sim_test, label='sim_test')
+    axs[1].set_title(f'Cosine Similarity')
+    axs[1].set_xlabel('Sample')
+    axs[1].set_ylabel('Similarity')
+    axs[1].legend()
+
+    # weights = model.get_weights()[0]
+    # axs[2].imshow(weights)
+    # axs[2].set_title(f'Weight Matrix')
+
+    fig.suptitle(f'{expt_name} Results')
+    plt.show()
+
+    print(model.summary())
+
+
+def load_dataset():
+    with open("dataset/wikiFA_docvecs_dim100.pkl", "rb") as f:
+        dataset = pickle.load(f)
+    return dataset
+
+# Calculate cosine similarity
+
+
+def cosine_similarity(vec1, vec2):
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+
+if __name__ == "__main__":
+    main()
---
+++ wikipedia-Data_to_DB-main/extractFA.py
@@ -0,0 +1,30 @@
+"""
+wiki_freatured_articlesを整理して, TopicList.txtへ書きこむ
+書き込んだものをTopicへテキストファイルとしてまとめる
+"""
+import codecs
+import os
+
+os.mkdir('Topic')
+# with open("wikipedia_featured_articles.txt", "r", encoding="utf-8", errors="ignore") as r:
+with open("wikipedia_featured_articles.txt", "r", encoding="utf-8") as r:
+    with codecs.open("TopicList.txt", "w", encoding="utf-8") as w: # utf-8で書き込む
+        content = r.readlines()
+        for line in content:
+            # 行頭が=のとき、トピックに関する処理を行う
+            if line[0] == "=":
+                line = line.strip("=")
+                line = line.split("=")
+                line = line[0].strip()
+                w.write(line + "\n")
+
+                print(line)
+                fileName = "Topic/" + line + ".txt"
+                print(fileName)
+                f = codecs.open(fileName, "w", encoding='utf-8')
+            # 記事に対する処理
+
+            elif line[0:19] == "{{FA/BeenOnMainPage" or line[0:2] == "[[":
+                line = line.split("[[")
+                line = line[1].split("]]")
+                f.write(line[0] + "\n")
